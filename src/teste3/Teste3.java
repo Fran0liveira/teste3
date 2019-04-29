8536d7b79366d0ac33919ce3c0547f548ab5dfba
Fran0liveira <=>
2019-04-28 22:04:48 -0300

    primeiro commit

---
+++ src/teste3/Teste3.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kikol
+ */
+public class Teste3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner tec =new Scanner(System.in);
+        
+        System.out.println("Digite um número= ");
+        int a = tec.nextInt();
+        System.out.println("Digite um número= ");
+        int b = tec.nextInt();
+        
+        int resposta = a * b;
+        
+        System.out.printf("O produto entre %d e %d é = %d ", a, b, resposta);
+        
+    }
+    
+}
